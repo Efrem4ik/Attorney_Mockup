164625ce0cd259f083a7a7866cbacb29588feac8
Ivan <37977990+Efrem4ik@users.noreply.github.com>
2019-09-15 09:57:26 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-$('.slider__items').slick();
+$('.slider__items').slick({
+  // centerMode: true,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$('.header__menu-btn').on("click", function () {
+  $('.header__menu > ul').slideToggle();
+});
